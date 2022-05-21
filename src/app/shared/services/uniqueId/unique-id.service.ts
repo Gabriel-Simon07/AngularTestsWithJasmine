ba48ef02cdbf14db1ed061cea69306752cb951a4
Gabriel Simon <gabrielsimon775@gmail.com>
2022-05-21 14:52:00 -0300

    First commit with UniqueId file

---
+++ src/app/shared/services/uniqueId/unique-id.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UniqueIdService {
+
+  private numberOfGenerateIds = 0;
+
+  generateUniqueIdWithPrefix(prefix: string): string {
+    if(!prefix) {
+      throw Error('Prefix can not be empty');
+    }
+    const uniqueId = this.generateUniqueId();
+    this.numberOfGenerateIds++;
+    return `${prefix}-${uniqueId}`;
+  }
+
+  private generateUniqueId(): string {
+    return uuidv4;
+  }
+
+  getNumberOfGenerateUniqueIds(): number {
+    return this.numberOfGenerateIds;
+  }
+}
