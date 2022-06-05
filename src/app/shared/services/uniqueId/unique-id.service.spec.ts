23c64f279484451941c06a6370684d7579fa7f76
Gabriel Simon <gabrielsimon775@gmail.com>
2022-06-05 20:28:19 -0300

    One more test implemented

--- src/app/shared/services/uniqueId/unique-id.service.spec.ts
+++ src/app/shared/services/uniqueId/unique-id.service.spec.ts
@@ -4,6 +4,15 @@ describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate ids when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for(let i =0; i <50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
   });
 });
